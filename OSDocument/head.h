b60b5a53b4e326dfebff62012922d543c8b74baf
Haochun Liang <1181558801@qq.com>
2019-06-19 09:20:18 +0800

    something changes

--- OSDocument/head.h
+++ OSDocument/head.h
@@ -1,1 +1,33 @@
 #pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+struct File
+{
+	int ID;
+	string Type;
+	int Long;
+	int Limit[7];
+	int User;
+
+
+
+};
+
+
+struct MyStruct
+{
+
+};
+
+
+struct User
+{
+
+};
+
+
+
+
