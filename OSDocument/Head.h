5dafe4e4d3557bc3806df0922d1f51cc0163539c
竹里沨笙月上门 <38268730+zhulifengsheng@users.noreply.github.com>
2019-06-19 16:27:04 +0800

    head文件

--- OSDocument/Head.h
+++ OSDocument/Head.h
@@ -9,6 +9,7 @@ struct Root_file //根目录
 {
 	int ID;
 	//其他信息
+	string File_name;//文件名称
 	string Type; //文件类型
 	int Length; //文件大小
 	int Limit[7]; //权限
@@ -18,13 +19,7 @@ struct Root_file //根目录
 	int File_address[100]; //文件占用块数
 };
 
-vector<Root_file> Base_table;
-
-
-
-
-
-
+extern vector<Root_file> Base_table;
 
 struct Files
 {
@@ -53,8 +48,12 @@ struct User
 {
 	string Name; //用户名
 	string Pwd; //密码
-}Users[8];
+};
+extern User Users[8];
 
+void callback(int num);
+void init_super_block(int Free_size, int Memory_size);
+int allocate();
 
-
-
+extern int BOOT_i = 0;
+extern queue<int> Reuse_file;
